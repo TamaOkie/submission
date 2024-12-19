a4f533aa53072218aff0e1e3516effce6a242c12
tamaokey <tamaoki2@jcom.zaq.ne.jp>
2024-12-19 15:27:22 +0900

    Step1の課題提出

---
+++ Kadai_step1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+####### 課題　ステップ１ #######
+echo "パスワードマネージャーへようこそ！"
+read -p "サービス名を入力してください：" svc
+read -p "ユーザー名を入力してください：" user
+read -p "パスワードを入力してください：" paswd
+
+# キー有力をtxtファイルに保管(追記式)
+echo $svc,$user,$paswd >> kanri.txt
+
+
+# 入力が完了したら
+echo "Thank you!"
